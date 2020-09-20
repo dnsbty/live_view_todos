e97c0007fa544afeaeeb65a60ee5c8ce635a54ce
Dennis Beatty <dennis@dcbeatty.com>
2020-09-20 12:17:27 -0600

    create a todo

--- lib/live_view_todos_web/live/todo_live.ex
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -3,6 +3,16 @@ defmodule LiveViewTodosWeb.TodoLive do
   alias LiveViewTodos.Todos
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, todos: Todos.list_todos())}
+    {:ok, fetch(socket)}
+  end
+
+  def handle_event("add", %{"todo" => todo}, socket) do
+    Todos.create_todo(todo)
+
+    {:noreply, fetch(socket)}
+  end
+
+  defp fetch(socket) do
+    assign(socket, todos: Todos.list_todos())
   end
 end
